b61136e7019df3745682c7c53e6bf81427d4e341
Peng Lv <owdely@vip.163.com>
2017-03-25 09:41:28 +0800

    添加项目文件。

---
+++ GetCorlor/Main.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="GetCorlor.Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <input type="file" id="OriDataFile" name="OriDataFile" runat="server" />
+            <asp:Button ID="btn_submit" Text="Upload" runat="server" onclick="btn_submit_Click" />           
+        </div>
+        <div>
+            <asp:Label ID="labAll" runat="server" Text=""></asp:Label>
+        </div>
+        <div>
+            <asp:TextBox ID="tbColor" runat="server" Height="421px" ReadOnly="True" TextMode="MultiLine" Width="409px"></asp:TextBox>
+        </div>       
+    </form>
+</body>
+</html>
---
+++ GetCorlor/Main.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+namespace GetCorlor
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        public int[,,] RGBdata;
+        public int[,,] intRGB;
+        public int intAllC;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_submit_Click(object sender, EventArgs e)
+        {
+            tbColor.Text = "";
+            if (OriDataFile.PostedFile != null && OriDataFile.PostedFile.ContentLength > 0)
+            {
+                string fn = System.IO.Path.GetFileName(OriDataFile.PostedFile.FileName);
+                string SaveLocation = Server.MapPath("Upload") + "\\" + fn;
+                try
+                {
+                    OriDataFile.PostedFile.SaveAs(SaveLocation);
+                    //Response.Write("The file has been uploaded.");
+                    //ShowMsgHelper.AlertMsg("The file has been uploaded.");
+                    GetColor(SaveLocation);
+                    labAll.Text = "It have " + intAllC + "Pixs";
+                    for(int R = 0; R < 256; R++)
+                    {
+                        for(int G = 0; G < 256; G++)
+                        {
+                            for(int B = 0; B < 256; B++)
+                            {
+                                if(intRGB[R,G,B] != 0)
+                                {
+                                    tbColor.Text = tbColor.Text + "R:" + R + "G:" + G + "B:" + B + "::" + intRGB[R, G, B] + "\r\n";
+                                }
+                            }
+                        }
+                    }
+                    //ShowMsgHelper.Alert_Error("Success.");
+                }
+                catch (Exception ex)
+                {
+                    //Response.Write("Error: " + ex.Message);
+                    //ShowMsgHelper.Alert_Error("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                //Response.Write("Please select a file to upload.");
+                //ShowMsgHelper.Alert_Error("Please select a file to upload.");
+            }
+        }
+
+        private void GetColor(string strAdd)
+        {
+            try
+            {
+                Bitmap curBitmap = (Bitmap)System.Drawing.Image.FromFile(strAdd);
+                int intW = curBitmap.Width;
+                int intH = curBitmap.Height;
+                RGBdata = new int[intW, intH, 3];
+                intRGB = new int[256, 256, 256];
+                intAllC = 0;
+                for (int x = 0; x < intW; x++)
+                {
+                    for (int y = 0; y < intH; y++)
+                    {
+                        Color c = curBitmap.GetPixel(x, y);
+                        RGBdata[x, y, 0] = c.R;
+                        RGBdata[x, y, 1] = c.G;
+                        RGBdata[x, y, 2] = c.B;
+                        intRGB[c.R, c.G, c.B] = intRGB[c.R, c.G, c.B] + 1;
+                        intAllC++;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
